0a54b5228a00e0bd291441635dc23d4df04e843e
zulya <zulya.h95@gmail.com>
2020-02-16 18:27:58 +0300

    relocate some files, change project structure;

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
-let gulp      = require('gulp'), // Подключаем Gulp
+let gulp        = require('gulp'), // Подключаем Gulp
     sass        = require('gulp-sass'), //Подключаем Sass пакет,
     browserSync = require('browser-sync'); // Подключаем Browser Sync
 
-gulp.task('sass', function(){ // Создаем таск Sass
+gulp.task('sass', function() { // Создаем таск Sass
     return gulp.src('app/sass/**/*.sass') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('app/js/common.js')
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('code', function() {
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
@@ -21,4 +31,5 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 gulp.task('watch', function() {
     gulp.watch('app/sass/**/*.sass', gulp.parallel('sass')); // Наблюдение за sass файлами
 });
+
 gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
